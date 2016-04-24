7424444f274f638ba97508a0d629f6bcb81e87a5
Daniil Belyakov <dnl.blkv@gmail.com>
2016-04-24 17:13:44 +0200

    replace tabs with spaces in create.js; fix if-else blocks styling

--- create.js
+++ create.js
@@ -9,57 +9,61 @@ var settings = JSON.parse(settingsFile);
 var visual_recognition = watson.visual_recognition(settings);
 
 var params = {
-	name: 'ChestFront',
-	positive_examples: fs.createReadStream('./goodsampleschest.zip'),
-	negative_examples: fs.createReadStream('./goodsamplesknee.zip')
+  name: 'ChestFront',
+  positive_examples: fs.createReadStream('./goodsampleschest.zip'),
+  negative_examples: fs.createReadStream('./goodsamplesknee.zip')
 };
 
 visual_recognition.createClassifier(params, 
-	function(err, response) {
-   	 if (err)
-      		console.log(err);
-    	 else
-   		console.log(JSON.stringify(response, null, 2));
+  function(err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.stringify(response, null, 2));
+    }
 });
 
 params = {
-	name: 'KneeFront',
-	positive_examples: fs.createReadStream('./goodsamplesknee.zip'),
-	negative_examples: fs.createReadStream('./goodsampleschest.zip')
+  name: 'KneeFront',
+  positive_examples: fs.createReadStream('./goodsamplesknee.zip'),
+  negative_examples: fs.createReadStream('./goodsampleschest.zip')
 };
 
 visual_recognition.createClassifier(params, 
-	function(err, response) {
-   	 if (err)
-      		console.log(err);
-    	 else
-   		console.log(JSON.stringify(response, null, 2));
+  function(err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.stringify(response, null, 2));
+    }
 });
 
 params = {
-	name: 'GoodChestFront',
-	positive_examples: fs.createReadStream('./goodsampleschest.zip'),
-	negative_examples: fs.createReadStream('./badsampleschest.zip')
+  name: 'GoodChestFront',
+  positive_examples: fs.createReadStream('./goodsampleschest.zip'),
+  negative_examples: fs.createReadStream('./badsampleschest.zip')
 };
 
 visual_recognition.createClassifier(params, 
-	function(err, response) {
-   	 if (err)
-      		console.log(err);
-    	 else
-   		console.log(JSON.stringify(response, null, 2));
+  function(err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.stringify(response, null, 2));
+    }
 });
 
 params = {
-	name: 'GoodKneeFront',
-	positive_examples: fs.createReadStream('./goodsamplesknee.zip'),
-	negative_examples: fs.createReadStream('./badsamplesknee.zip')
+  name: 'GoodKneeFront',
+  positive_examples: fs.createReadStream('./goodsamplesknee.zip'),
+  negative_examples: fs.createReadStream('./badsamplesknee.zip')
 };
 
 visual_recognition.createClassifier(params, 
-	function(err, response) {
-   	 if (err)
-      		console.log(err);
-    	 else
-   		console.log(JSON.stringify(response, null, 2));
+  function(err, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.stringify(response, null, 2));
+    }
 });
